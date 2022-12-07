4f119554a1248c62dc2d0ebdabf1ff05fecd138c
Kelly Zhang <kelly@pelican.local>
2022-12-06 23:50:18 -0500

    setting up entry splash image & card buttons

--- js/welcome.js
+++ js/welcome.js
@@ -1,10 +1,19 @@
 const splash_overlay = document.getElementById("splash-overlay");
+const welcome_splash_1 = document.getElementById("welcome-splash-1");
+const welcome_splash_2 = document.getElementById("welcome-splash-2");
 
 
 // temp callback to remove splash overlay screen for now.
 // probably should toggle the active state when card data loads in
 document.body.onload = function () {
     setTimeout(function () {
+        window.scrollTo(0, 0)
         splash_overlay.classList.remove("active");
+        welcome_splash_1.classList.add("active");
     }, 2000);
+}
+
+function toggleWelcomeTwo () {
+    welcome_splash_1.classList.add("scroll-up");
+    welcome_splash_2.classList.add("active");
 }
