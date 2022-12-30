ca7426575629a978be424af8275c776d2ec0a645
bon <bon@bon-v.tube>
2022-12-30 02:18:43 -0500

    removing top level collection.html & nav.html. commenting. nav fix to display outline on keyboard focus

--- js/main.js
+++ js/main.js
@@ -39,17 +39,19 @@ function setupNavClickEvents () {
   let closeButton = document.getElementById("close-nav-menu");
   let navElement = document.getElementsByTagName("nav")[0];
   function openNavMenu (event) {
+    // for css transition purposes, .active sets visibility state, while .open sets transform & opacity
     this.classList.add("active");
-    setTimeout(() => {
-      this.classList.add("open");
-    }, 250);
+    this.classList.add("open");
   }
   
   function closeNavMenu (event) {
+    // removing both at once causes the transition out to be sudden, so separate w/ timeout
+    // transition out the transform & opacity first
     this.classList.remove("open");
     setTimeout(() => {
+      // then remove visibility
       this.classList.remove("active");
-    }, 250);
+    }, 250); // 0.25s is the transition timing (see nav.css)
   }
 
   mobileButton.onclick = openNavMenu.bind(navElement);
