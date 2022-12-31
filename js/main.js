6e71e24d7ace6812ed663068cc4ca0a5c01a620b
Kelly Zhang <kezhang@wpi.edu>
2022-12-30 21:34:15 -0500

    renaming #nav-button to #nav-hamburger. separating nav click events to separate file.

--- js/main.js
+++ js/main.js
@@ -31,33 +31,6 @@ function getCSVData(callback = undefined) {
   });
 }
 
-/**
- * Setup click events for hamburger nav
- */
-function setupNavClickEvents () {
-  let mobileButton = document.getElementById("nav-button");
-  let closeButton = document.getElementById("close-nav-menu");
-  let navElement = document.getElementsByTagName("nav")[0];
-  function openNavMenu (event) {
-    // for css transition purposes, .active sets visibility state, while .open sets transform & opacity
-    this.classList.add("active");
-    this.classList.add("open");
-  }
-  
-  function closeNavMenu (event) {
-    // removing both at once causes the transition out to be sudden, so separate w/ timeout
-    // transition out the transform & opacity first
-    this.classList.remove("open");
-    setTimeout(() => {
-      // then remove visibility
-      this.classList.remove("active");
-    }, 250); // 0.25s is the transition timing (see nav.css)
-  }
-
-  mobileButton.onclick = openNavMenu.bind(navElement);
-  closeButton.onclick = closeNavMenu.bind(navElement);
-}
-
 //Classifies all cards based on their rarity and saves them
 //in groups inside cards_by_rarity with the following format:
 //{
@@ -78,7 +51,6 @@ function getCardsByRarity() {
 }
 
 async function main() {
-  setupNavClickEvents();
   await defineCardComponent();
   getCSVData(async (cards_data) => {
     switch (CURRENT_PAGE) {
