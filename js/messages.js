98df459738abba38854f8f7aab0ace19fcd965c4
bon <bon@bon-v.tube>
2023-01-07 01:41:34 -0500

    opting to use li over article for messages

--- js/messages.js
+++ js/messages.js
@@ -17,14 +17,11 @@ function renderMessages () {
     let messageContainer = document.getElementById("artist-writer-messages");
     for (var i = 1; i < 100; i++) {
       let messageNode = document.createElement("li");
-      let messageArticle = document.createElement("article");
       // TODO: Dummy data for now. Alternate roles for now as a test. This should be pulled from a csv somewhere.
       let userRole = i % 2 === 0 ? "Artist" : "Writer";
       let userSocial = `user_social_${i}`;
       let h3String = [userRole, userSocial].join(" | ");
-      messageNode.tabIndex = 0;
-      messageArticle.tabIndex = 0;
-      messageArticle.classList.add("artist-writer-message");
+      messageNode.classList.add("artist-writer-message");
       let messageUserHeader = document.createElement("h2");
       let messageUserInfo = document.createElement("h3");
       let messageUserMessage = document.createElement("p");
@@ -33,11 +30,10 @@ function renderMessages () {
       messageUserInfo.innerText = h3String;
       messageUserMessage.innerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas. Blandit massa enim nec dui nunc mattis enim ut tellus. Cursus euismod quis viverra nibh cras. Porttitor massa id neque aliquam vestibulum morbi blandit cursus.`;
       
-      messageArticle.appendChild(messageUserHeader);
-      messageArticle.appendChild(messageUserInfo);
-      messageArticle.appendChild(messageUserMessage);
-    
-      messageNode.appendChild(messageArticle);
+      messageNode.appendChild(messageUserHeader);
+      messageNode.appendChild(messageUserInfo);
+      messageNode.appendChild(messageUserMessage);
+      
       messageContainer.appendChild(messageNode);
     }
 }
