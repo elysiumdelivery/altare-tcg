41a3e786e22ebf72711d72ffacf4f217f7078ae5
bon <bon@bon-v.tube>
2023-01-12 23:36:54 -0500

    adding in contributor messages & credits csv

--- js/messages.js
+++ js/messages.js
@@ -1,46 +1,98 @@
-const CSV_FILENAME = "../Test Card List CSV.csv";
+const MESSAGES_CSV = "../contributor_data.csv";
+const CREDITS_CSV = "../credits_data.csv";
+const pathname = window.location.pathname;
+const CURRENT_PAGE = pathname.slice(pathname.lastIndexOf("/"), pathname.length);
 
-function getCSVData(callback = undefined) {
-    Papa.parse(CSV_FILENAME, {
-      download: true,
-      //To treat the first row as column titles
-      header: true,
-      complete: (result) => {
-        if (callback) {
-          callback(result);
-        }
-      },
-    });
-  }
+function getCSVData(page, callback = undefined) {
+    let filename;
+    switch (page) {
+        case "/contributor-board.html":
+            filename = MESSAGES_CSV;
+            break;
+        case "/credits.html":
+            filename = CREDITS_CSV;
+            break;
+    }
 
-function renderMessages () {
-    let messageContainer = document.getElementById("artist-writer-messages");
-    for (var i = 1; i < 100; i++) {
-      let messageNode = document.createElement("li");
-      // TODO: Dummy data for now. Alternate roles for now as a test. This should be pulled from a csv somewhere.
-      let userRole = i % 2 === 0 ? "Artist" : "Writer";
-      let userSocial = `user_social_${i}`;
-      let h3String = [userRole, userSocial].join(" | ");
-      messageNode.classList.add("artist-writer-message");
-      let messageUserHeader = document.createElement("h2");
-      let messageUserInfo = document.createElement("h3");
-      let messageUserMessage = document.createElement("p");
-      
-      messageUserHeader.innerText = `user_name_${i}`;
-      messageUserInfo.innerText = h3String;
-      messageUserMessage.innerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus cras adipiscing enim eu turpis egestas. Blandit massa enim nec dui nunc mattis enim ut tellus. Cursus euismod quis viverra nibh cras. Porttitor massa id neque aliquam vestibulum morbi blandit cursus.`;
-      
-      messageNode.appendChild(messageUserHeader);
-      messageNode.appendChild(messageUserInfo);
-      messageNode.appendChild(messageUserMessage);
-      
-      messageContainer.appendChild(messageNode);
+    if (filename) {
+        Papa.parse(filename, {
+          download: true,
+          //To treat the first row as column titles
+          header: true,
+          complete: (result) => {
+            if (callback) {
+              callback(result.data);
+            }
+          },
+        });
     }
 }
 
+
+function renderMessages (message_data) {
+    let messageContainer = document.getElementById("artist-writer-messages");
+    console.log(message_data);
+    for (var i = 0; i < message_data.length; i++) {
+        let message = message_data[i];
+        let messageNode = document.createElement("li");
+        // TODO: Dummy data for now. Alternate roles for now as a test. This should be pulled from a csv somewhere.
+        //   let userRole = i % 2 === 0 ? "Artist" : "Writer";
+        messageNode.classList.add("artist-writer-message");
+        let messageUserHeader = document.createElement("h2");
+        let messageUserInfo = document.createElement("h3");
+        let messageUserMessage = document.createElement("p");
+        
+        messageUserHeader.innerText = message["Participant Name"];
+        if (message["Twitter"]) {
+            let userSocial = document.createElement("a");
+            userSocial.href = `https://twitter.com/${message["Twitter"]}`;
+            userSocial.textContent = message["Twitter"];
+            messageUserInfo.append(userSocial);
+        }
+        messageUserMessage.innerText = message["Birthday Message"];
+        
+        messageNode.appendChild(messageUserHeader);
+        messageNode.appendChild(messageUserInfo);
+        messageNode.appendChild(messageUserMessage);
+
+        messageContainer.appendChild(messageNode);
+    }
+}
+
+function renderCredits (credits_data) {
+    let creditsContainer = document.getElementById("credits-column");
+    credits_data.forEach((credit) => {
+        if (credit["Staff name"]) {
+            let creditListing = document.createElement("li");
+    
+            let creditName = document.createElement("h3");
+            creditName.textContent = credit["Staff name"];
+            creditListing.append(creditName);
+            if (credit["Twitter"]) {
+                let creditTwitter = document.createElement("a");
+                creditTwitter.href = `https://twitter.com/${credit["Twitter"]}`;
+                creditTwitter.textContent = credit["Twitter"];
+                creditListing.append(creditTwitter);
+            }
+            let creditRole = document.createElement("p");
+            creditRole.textContent = credit["Role"];
+            creditListing.append(creditRole);
+
+            creditsContainer.append(creditListing);
+        }
+    });
+}
+
 async function main() {
-    getCSVData(async (messages_data) => {
-        renderMessages();
+    getCSVData(CURRENT_PAGE, async (csv_data) => {
+        switch (CURRENT_PAGE) {
+        case "/contributor-board.html":
+            renderMessages(csv_data);
+            break;
+        case "/credits.html":
+            renderCredits(csv_data);
+            break;
+        }
     });
 }
   
