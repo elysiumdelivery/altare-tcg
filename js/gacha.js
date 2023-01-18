1c526d057c9a1a6985523f7aec8be1bc48cadcc1
bon <bon@bon-v.tube>
2023-01-18 01:07:26 -0500

    comments

--- js/gacha.js
+++ js/gacha.js
@@ -1,6 +1,7 @@
 import { renderCards } from "./cards.js";
 import { cards_by_rarity } from "./main.js";
 
+// Potentially multiple gacha buttons, so look for all
 export const GACHA_BUTTONS = document.getElementsByClassName("gacha-button");
 
 //Card slots on each pull, representing the percent chance of getting a card of that rarity in each slot.
@@ -150,7 +151,7 @@ export function pullAndRenderCards(render_location) {
   renderCards(pulled, render_location, true);
   render_location.insertAdjacentHTML(
     "beforeend",
-    `<span class="visually-hidden">No more cards in this pack. Roll again?</span>`
+    `<span id="roll-again-prompt" class="visually-hidden">No more cards in this pack.</span>`
   );
   
   // Add prompt to roll again
@@ -160,9 +161,11 @@ export function pullAndRenderCards(render_location) {
   gachaRollAgain.insertAdjacentHTML("beforeend", `<img src="../images/slimelooptransparent.gif" alt="Cultare slime bouncing up and down"/>`);
   
   let gachaRollAgainButton = document.createElement("button");
+  gachaRollAgainButton.id = "gacha-button-roll-again";
   gachaRollAgainButton.className = "gacha-button";
   gachaRollAgainButton.type = "button";
-  gachaRollAgainButton.textContent = "Roll again";
+  gachaRollAgainButton.textContent = "Roll Again?";
+  gachaRollAgainButton.setAttribute("aria-labelledby", "roll-again-prompt gacha-button-roll-again");
   gachaRollAgainButton.onclick = (event) => pullAndRenderCards(document.getElementById("card-list"));
 
   gachaRollAgain.append(gachaRollAgainButton);
